8371e0b9009a2c030b5dc34473c016f3131bd873
Daxeel Soni <daxeel_soni4444@yahoo.com>
2016-03-17 10:22:03 +0530

    Now plaotting graph is available

--- twisis.py
+++ twisis.py
@@ -11,15 +11,18 @@
 # Copyright (c) 2016, Daxeel Soni
 # All rights reserved.
 
+# Import required modules
 import tweepy
 import urllib
+import plotly
+from plotly.graph_objs import Bar, Layout
 
 class Twisis(object):
 	"""Python module for twitter Analysis"""
 
 	# Connecting twitter API
-	auth = tweepy.OAuthHandler('jiHQF74FShbbOEkg1pYX5bZj1', 'dIKY6SDkfGqyLCja9zsf4qtw7jVOFD2aG4cdI6FQs2sJtABxEH')
-	auth.set_access_token('2547263228-pKM4XiZeRzOFCu9yREehZtL1vsJojW0jCCXEyCy', 'QYLMt9MpBJeKVOmzyap1dAMrv6aNZsd8mNbTcUI86sRnh')
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
 	api = tweepy.API(auth)
 
 	def __init__(self, hashtag, no_tweets):
@@ -27,7 +30,7 @@ class Twisis(object):
 		self.hashtag = hashtag
 		self.no_tweets = no_tweets
 		
-	def hashtag_analysis(self):
+	def hashtag_analysis(self, plot, a_type):
 		final_data = []
 
 		for each in range(self.no_tweets):
@@ -36,26 +39,55 @@ class Twisis(object):
 
 			# Getting tweet
 			tweet = Twisis.api.search('#'+self.hashtag)[each].text
+			tweet = tweet.encode('utf-8')
 
-			try:
-				data = urllib.urlencode({"text": tweet}) 
-				senti_request = urllib.urlopen("http://text-processing.com/api/sentiment/", data)
-				senti_response = eval(senti_request.read())
+			data = urllib.urlencode({"text": tweet}) 
+			senti_request = urllib.urlopen("http://text-processing.com/api/sentiment/", data)
+			senti_response = eval(senti_request.read())
 
-				label = senti_response['label']
-				positive_number = senti_response['probability']['pos']
-				negative_number = senti_response['probability']['neg']
-				neutral_number = senti_response['probability']['neutral']
+			label = senti_response['label']
+			positive_number = senti_response['probability']['pos']
+			negative_number = senti_response['probability']['neg']
+			neutral_number = senti_response['probability']['neutral']
 
-				temp_dict['tweet'] = tweet
-				temp_dict['label'] = label
-				temp_dict['positive_number'] = positive_number
-				temp_dict['negative_number'] = negative_number
-				temp_dict['neutral_number'] = neutral_number
+			temp_dict['tweet'] = tweet
+			temp_dict['label'] = label
+			temp_dict['positive_number'] = positive_number
+			temp_dict['negative_number'] = negative_number
+			temp_dict['neutral_number'] = neutral_number
 
-				final_data.append(temp_dict)
-			except:
-				print tweet
+			final_data.append(temp_dict)
+
+		if plot==True:
+			# Get positivity number of tweets for y axis
+			pos_data = []
+			plot_title = ""
+			for each in final_data:
+				if a_type=='pos':
+					pos_data.append(each['positive_number'])
+					plot_title = "Positive sentimental analytics for tweets of #"+self.hashtag
+				elif a_type=='neg':
+					pos_data.append(each['negative_number'])
+					plot_title = "Negative sentimental analytics for tweets of #"+self.hashtag
+				elif a_type=='neu':
+					pos_data.append(each['neutral_number'])
+					plot_title = "Neutral sentimental analytics for tweets of #"+self.hashtag
+
+			# Create label list for x axix
+			labels = []
+			for each in range(len(pos_data)):
+				labels.append("Tweet-" + str(each+1))
+
+			# Plot the graph
+			plotly.offline.plot({
+				"data": [
+				    Bar(x=labels, y=pos_data)
+				],
+				"layout": Layout(
+				    title=plot_title
+				)
+			})
+			
 
 		return final_data
 
