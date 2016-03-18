e5e160b28d3bd542d48e51d7be9e46daf667a078
Daxeel Soni <daxeel_soni4444@yahoo.com>
2016-03-18 19:49:24 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='twisis',
+      version='1.0',
+      description='A python module for twitter Analysis',
+      author='Daxeel Soni',
+      author_email='sayhi@daxeelsoni.in',
+      url='http://www.daxeelsoni.in',
+      keywords = ['twisis', 'twitter', 'tweet', 'analytics', 'sentimental'],
+      py_modules=['twisis'],
+     )
